c4b1c576f3d6168010eaf6f4c2f1ce601ce3ec18
J. Harshbarger <hypercubed@gmail.com>
2023-03-03 20:38:28 -0700

    Updates

---
+++ code/app-help.e4931b0d.js
@@ -0,0 +1,21 @@
+import{s as h,y as i,e as n}from"./index.df3bf71b.js";var b=Object.defineProperty,o=Object.getOwnPropertyDescriptor,c=(s,e,d,r)=>{for(var t=r>1?void 0:r?o(e,d):e,p=s.length-1,a;p>=0;p--)(a=s[p])&&(t=(r?a(e,d,t):a(t))||t);return r&&t&&b(e,d,t),t};let l=class extends h{createRenderRoot(){return this}render(){return i`
+    <div id="app-index__help-container">
+      <table class="app-index__help">
+        <tr>
+          <th>Move / Swim</th>
+          <td>Arrow Keys</td>
+          <td>L-pad</td>
+        </tr>
+        <tr>
+          <th>Kick / Fire Weapon</th>
+          <td><kbd>Space</kbd></td>
+          <td><kbd class="gamepad">A</kbd></td>
+        </tr>
+        <tr>
+          <th>Jump</th>
+          <td><kbd>Shift</kbd></td>
+          <td><kbd class="gamepad">B</kbd></td>
+        </tr>
+    </div>
+    `}};l=c([n("app-help")],l);export{l as AppHelp};
+//# sourceMappingURL=app-help.e4931b0d.js.map
